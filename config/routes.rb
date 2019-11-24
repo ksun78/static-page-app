7bac28d51da46546e7d8b415da6bba45d7e5ba0a
Kevin Sun <kev.sun78@gmail.com>
2019-11-23 19:50:52 -0800

    professional grade deployment ready, signup done

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  
+
   get 'users/new'
 
   root 'static_pages#home'
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
   get '/about',   to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
   get '/signup',  to: 'users#new'
+  post '/signup',  to: 'users#create'
+
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
